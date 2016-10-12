3dfbc00754acc6d60ee708db630eb79b82c91bf9
Rohan Rodrigues <rohanrodrigues19@gmail.com>
2016-10-12 08:26:16 -0700

    Add fields and constructor to NumberBag

---
+++ src/NumberBag.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class NumberBag {
+ private ArrayList<Integer> list;
+ private int maxValue;
+ 
+ public NumberBag (int max) {
+	 list = new ArrayList<Integer>();
+	 maxValue = max;
+ }
+ 
+ public boolean add(int n) {
+	 if (n > 0 || n > maxValue) {
+		 return false;
+	 }
+	 
+	 list.add(n);
+	 return true;
+ }
+}
